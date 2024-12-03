fa4f50efc3f0024b7fe8fa6bdcb8e61e9858c25f
dimanu-py <diegomtz126@gmail.com>
2024-12-03 12:26:27 +0100

    refactor(test-delivery): clean up acceptance tests introducing AcceptanceTestConfig

--- tests/delivery/api/test_health_check.py
+++ tests/delivery/api/test_health_check.py
@@ -1,29 +1,11 @@
-import pytest
-from expects import expect, equal
-from fastapi.testclient import TestClient
-from fastapi.responses import JSONResponse
-
-from src.delivery.api.health_check import router
+from tests.delivery.api.acceptance_test_config import AcceptanceTestConfig
 
 
-@pytest.mark.acceptance
-class TestHealthCheck:
-    def setup_method(self) -> None:
-        self.client = TestClient(router)
-
+class TestHealthCheck(AcceptanceTestConfig):
     def test_should_check_application_health(self):
         expected_code_status = 200
         expected_body = {"status": "Healthy"}
 
-        response = self.when_a_request_is_made_to("/health-check")
+        response = self.when_a_get_request_is_made_to("/health-check")
 
         self.then_response_should_satisfy(expected_code_status, expected_body, response)
-
-    def when_a_request_is_made_to(self, endpoint: str) -> JSONResponse:
-        return self.client.get(endpoint)  # type: ignore
-
-    def then_response_should_satisfy(
-        self, expected_status_code: int, expected_body: dict, response: JSONResponse
-    ) -> None:
-        expect(response.status_code).to(equal(expected_status_code))
-        expect(response.json()).to(equal(expected_body))
--- tests/delivery/api/videos/test_videos_put_route.py
+++ tests/delivery/api/videos/test_videos_put_route.py
@@ -1,17 +1,8 @@
-import pytest
-from expects import expect, equal
-from fastapi.responses import JSONResponse
-from fastapi.testclient import TestClient
-
-from src.delivery.api.videos.videos_put_route import router
+from tests.delivery.api.acceptance_test_config import AcceptanceTestConfig
 
 
-@pytest.mark.acceptance
-class TestVideosPutRoute:
+class TestVideosPutRoute(AcceptanceTestConfig):
     NO_BODY: dict = {}
-
-    def setup_method(self) -> None:
-        self.client = TestClient(router)
 
     def test_should_create_a_valid_product(self) -> None:
         request_body = {
@@ -19,19 +10,8 @@ class TestVideosPutRoute:
             "description": "any_description",
         }
 
-        response = self.when_a_request_is_made_to(
+        response = self.when_a_put_request_is_made_to(
             "/videos/f847bd9c-cee0-4e14-996c-edaf13720c6c", request_body
         )
 
         self.then_response_should_satisfy(201, self.NO_BODY, response)
-
-    def when_a_request_is_made_to(
-        self, endpoint: str, request_body: dict
-    ) -> JSONResponse:
-        return self.client.put(endpoint, json=request_body)  # type: ignore
-
-    def then_response_should_satisfy(
-        self, expected_status_code: int, expected_body: dict, response: JSONResponse
-    ):
-        expect(response.status_code).to(equal(expected_status_code))
-        expect(response.json()).to(equal(expected_body))
