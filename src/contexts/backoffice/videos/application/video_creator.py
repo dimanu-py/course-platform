2a833d6b28caf3f0c4c7160ff0a0eebb43cc12ea
dimanu-py <diegomtz126@gmail.com>
2024-12-03 09:57:53 +0100

    feat(backoffice): implement VideoCreator use case

---
+++ src/contexts/backoffice/videos/application/video_creator.py
@@ -0,0 +1,16 @@
+from src.contexts.backoffice.videos.application.create_video_command import (
+    CreateVideoCommand,
+)
+from src.contexts.backoffice.videos.domain.video import Video
+from src.contexts.backoffice.videos.domain.video_repository import VideoRepository
+
+
+class VideoCreator:
+    repository: VideoRepository
+
+    def __init__(self, repository: VideoRepository) -> None:
+        self.repository = repository
+
+    def __call__(self, command: CreateVideoCommand) -> None:
+        video = Video.create(command.id, command.title, command.description)
+        self.repository.save(video)
--- tests/contexts/backoffice/videos/application/create_video_command_mother.py
+++ tests/contexts/backoffice/videos/application/create_video_command_mother.py
@@ -4,8 +4,8 @@ from src.contexts.backoffice.videos.application.create_video_command import (
 
 
 class CreateVideoCommandMother:
-    _VALID_ID = "8e197c6-0379-4142-acb7-9234f460ca6e"
+    _VALID_ID = "f847bd9c-cee0-4e14-996c-edaf13720c6c"
 
     @classmethod
     def with_valid_id(cls) -> CreateVideoCommand:
-        return CreateVideoCommand(cls._VALID_ID, "title", "description", 10)
+        return CreateVideoCommand(cls._VALID_ID, "title", "description")
