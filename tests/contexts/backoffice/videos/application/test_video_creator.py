36babe0f8216ebaa8aa0c047cbd5ef4579d94695
dimanu-py <diegomtz126@gmail.com>
2024-12-03 12:25:37 +0100

    refactor(test-backoffice): clean up unit tests introducing VideoModuleUnitTestConfig

--- tests/contexts/backoffice/videos/application/test_video_creator.py
+++ tests/contexts/backoffice/videos/application/test_video_creator.py
@@ -1,29 +1,22 @@
-import pytest
-from doublex import Spy
-from doublex_expects import have_been_called_with
 from expects import expect, raise_error
 
-from src.contexts.backoffice.videos.application.video_creator import VideoCreator
-from src.contexts.backoffice.videos.domain.video_repository import VideoRepository
 from tests.contexts.backoffice.videos.application.create_video_command_mother import (
     CreateVideoCommandMother,
 )
 from tests.contexts.backoffice.videos.domain.video_mother import VideoMother
+from tests.contexts.backoffice.videos.video_module_unit_test_config import (
+    VideoModuleUnitTestConfig,
+)
 
 
-@pytest.mark.unit
-class TestVideoCreator:
-    def setup_method(self) -> None:
-        self.repository = Spy(VideoRepository)
-        self.video_creator = VideoCreator(self.repository)
-
+class TestVideoCreator(VideoModuleUnitTestConfig):
     def test_should_create_a_valid_video(self) -> None:
         command = CreateVideoCommandMother.with_valid_id()
         video_to_save = VideoMother.from_request(command)
 
         self.video_creator(command)
 
-        expect(self.repository.save).to(have_been_called_with(video_to_save))
+        self.should_have_saved(video_to_save)
 
     def test_should_fail_to_create_video_with_invalid_id(self) -> None:
         command = CreateVideoCommandMother.with_invalid_id()
