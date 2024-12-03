27fb1b342e7459feb631043c20a4fd5e3b89dc2a
dimanu-py <diegomtz126@gmail.com>
2024-12-03 11:14:14 +0100

    refactor(backoffice): convert VideoRepository into an interface

--- src/contexts/backoffice/videos/domain/video_repository.py
+++ src/contexts/backoffice/videos/domain/video_repository.py
@@ -1,6 +1,9 @@
+from abc import ABC, abstractmethod
+
 from src.contexts.backoffice.videos.domain.video import Video
 
 
-class VideoRepository:
+class VideoRepository(ABC):
+    @abstractmethod
     def save(self, video: Video) -> None:
         raise NotImplementedError
