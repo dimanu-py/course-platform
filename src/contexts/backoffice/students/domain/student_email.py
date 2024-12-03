8825aff8789e49182a518625871dc3e108e92520
dimanu-py <diegomtz126@gmail.com>
2024-12-03 13:34:39 +0100

    test(backoffice): verify is not possible to create a student with invalid id nor invalid email

--- src/contexts/backoffice/students/domain/student_email.py
+++ src/contexts/backoffice/students/domain/student_email.py
@@ -1,7 +1,17 @@
+import re
+
+from src.contexts.backoffice.students.domain.invalid_email_format_error import (
+    InvalidEmailFormatError,
+)
 from src.contexts.shared.domain.value_objects.string_value_object import (
     StringValueObject,
 )
 
 
 class StudentEmail(StringValueObject):
-    pass
+    EMAIL_EXPRESSION = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+
+    def _validate(self, value: str) -> None:
+        super()._validate(value)
+        if re.match(self.EMAIL_EXPRESSION, value) is None:
+            raise InvalidEmailFormatError(value)
