9e13a5bc30578ca7800286d9344bda3cd915fa1e
dimanu-py <diegomtz126@gmail.com>
2024-12-03 16:36:52 +0100

    feat(delivery): add student create route to main application

--- src/delivery/api/main.py
+++ src/delivery/api/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
 from src.delivery.api import health_check
+from src.delivery.api.students import students_put_route
 from src.delivery.api.videos import videos_put_route
 
 app = FastAPI()
 
 app.include_router(health_check.router)
 app.include_router(videos_put_route.router)
+app.include_router(students_put_route.router)
