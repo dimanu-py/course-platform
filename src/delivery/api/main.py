24e7e554aace78869d267ea579acd1a6ef266908
dimanu-py <diegomtz126@gmail.com>
2024-12-03 12:24:37 +0100

    feat(delivery): add video put controller to main application

--- src/delivery/api/main.py
+++ src/delivery/api/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from src.delivery.api import health_check
+from src.delivery.api.videos import videos_put_route
 
 app = FastAPI()
 
 app.include_router(health_check.router)
+app.include_router(videos_put_route.router)
