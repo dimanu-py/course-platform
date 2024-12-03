3c9dd2558a44890c68c1a54dacba978ad1b7bd09
dimanu-py <diegomtz126@gmail.com>
2024-12-03 16:36:31 +0100

    refactor(delivery): use InMemoryStudentRepository temporarily

--- src/delivery/api/students/students_put_route.py
+++ src/delivery/api/students/students_put_route.py
@@ -5,7 +5,9 @@ from src.contexts.backoffice.students.application.create_student_command import 
     CreateStudentCommand,
 )
 from src.contexts.backoffice.students.application.student_creator import StudentCreator
-from src.contexts.backoffice.students.domain.student_repository import StudentRepository
+from src.contexts.backoffice.students.infra.in_memory_student_repository import (
+    InMemoryStudentRepository,
+)
 from src.delivery.api.students.student_create_request import CreateStudentRequest
 
 router = APIRouter(prefix="/students", tags=["Students"])
@@ -13,7 +15,7 @@ router = APIRouter(prefix="/students", tags=["Students"])
 
 @router.put("/{_id}")
 async def create_student(_id: str, request: CreateStudentRequest) -> JSONResponse:
-    student_creator = StudentCreator(repository=StudentRepository())
+    student_creator = StudentCreator(repository=InMemoryStudentRepository())
     command = CreateStudentCommand(_id, request.name, request.username, request.email)
 
     student_creator(command)
