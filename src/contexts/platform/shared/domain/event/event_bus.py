4e75d7c8e945a7e173e671e1b72daa192fd163eb
dimanu-py <diegomtz126@gmail.com>
2024-12-17 12:55:02 +0100

    refactor(shared): make EventBus abstract

--- src/contexts/platform/shared/domain/event/event_bus.py
+++ src/contexts/platform/shared/domain/event/event_bus.py
@@ -1,6 +1,9 @@
+from abc import ABC, abstractmethod
+
 from src.contexts.platform.shared.domain.event.domain_event import DomainEvent
 
 
-class EventBus:
+class EventBus(ABC):
+    @abstractmethod
     def publish(self, events: list[DomainEvent]) -> None:
         raise NotImplementedError
