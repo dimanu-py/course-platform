3230f7fafaca94ccaa6940afb60a4f1dfda6364f
dimanu-py <diegomtz126@gmail.com>
2024-12-17 11:41:39 +0100

    refactor(shared): inject RabbitMqConnection object into RabbitMqEventBus

--- tests/contexts/shared/infra/test_rabbit_mq_event_bus.py
+++ tests/contexts/shared/infra/test_rabbit_mq_event_bus.py
@@ -1,5 +1,11 @@
+from src.contexts.platform.shared.infra.event.rabbit_mq.rabbit_mq_connection import (
+    RabbitMqConnection,
+)
 from src.contexts.platform.shared.infra.event.rabbit_mq.rabbit_mq_event_bus import (
     RabbitMqEventBus,
+)
+from src.contexts.platform.shared.infra.event.rabbit_mq.rabbit_mq_settings import (
+    RabbitMqSettings,
 )
 from tests.contexts.platform.videos.domain.video_created_domain_event_mother import (
     VideoCreatedDomainEventMother,
@@ -7,10 +13,15 @@ from tests.contexts.platform.videos.domain.video_created_domain_event_mother imp
 
 
 class TestRabbitMqEventBus:
+    def setup_method(self) -> None:
+        self.client = RabbitMqConnection(
+            connection_settings=RabbitMqSettings(
+                user="admin", password="admin", host="localhost"
+            )
+        )
+
     def test_should_publish_events(self) -> None:
         event = VideoCreatedDomainEventMother.create()
-        producer = RabbitMqEventBus(
-            url="amqp://admin:admin@localhost/", exchange_name="domain_events"
-        )
+        producer = RabbitMqEventBus(client=self.client, exchange_name="domain_events")
 
         producer.publish([event])
